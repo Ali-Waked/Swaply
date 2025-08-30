5d1ac8c398b0f6131edfff4bf3ec8c15c759eb09
OSHENDI <107985018+OSHENDI@users.noreply.github.com>
2025-08-30 18:25:58 +0300

    Update WoringAlert.vue
    color tweak

--- src/components/front/global/WoringAlert.vue
+++ src/components/front/global/WoringAlert.vue
@@ -12,8 +12,8 @@ defineProps({
 </script>
 
 <template>
-  <div class="p-4 rounded-lg bg-amber-100 border border-amber-300">
-    <h5 class="title text-amber-600 font-[400] mb-1">{{ title }}</h5>
-    <p class="text text-amber-600 text-[14px]">{{ text }}</p>
+  <div class="p-4 rounded-lg bg-[#fefce8] border border-[#fab005]">
+    <h5 class="title text-[#89530c] font-[500] mb-1">{{ title }}</h5>
+    <p class="text text-[#89530c] text-[14px]">{{ text }}</p>
   </div>
 </template>
