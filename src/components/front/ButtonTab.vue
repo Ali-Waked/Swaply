3a5adaaf70c4b19b244d4655b4aee2fae7da0e24
OSHENDI <107985018+OSHENDI@users.noreply.github.com>
2025-09-04 14:01:52 +0300

    Update ButtonTab.vue
    some styling touches

--- src/components/front/ButtonTab.vue
+++ src/components/front/ButtonTab.vue
@@ -21,9 +21,9 @@ const props = defineProps({
     :type="type"
     class="border-2 flex-1 transition-all font-[400] flex items-center justify-center gap-1"
     :class="{
-      ' bg-white border-gray-300 text-gray-900  dark:bg-blue-600 dark:text-white dark:hover:bg-blue-700 dark:border-blue-700':
+      'bg-white border-gray-300 text-gray-900 dark:bg-blue-600 dark:text-white dark:hover:bg-blue-700 dark:border-blue-700 focus:hover:bg-white dark:focus:hover-bg-blue-600':
         isActive,
-      'border-transparent text-gray-600 dark:text-gray-50 dark:hover:bg-white/45':
+      'border-transparent text-gray-600 dark:text-gray-50 hover:bg-white dark:hover:bg-white/45':
         !isActive,
     }"
   >
