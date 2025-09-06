6f09a88504f4df616c47452acfe76c92249aeac2
OSHENDI <107985018+OSHENDI@users.noreply.github.com>
2025-09-06 12:36:29 +0300

    Update HeaderTitle.vue

--- src/components/front/global/HeaderTitle.vue
+++ src/components/front/global/HeaderTitle.vue
@@ -10,7 +10,7 @@ defineProps({
 </script>
 
 <template>
-  <h3 class="title font-[500] text-[24px] text-black dark:text-white">
+  <h3 class="title font-[500] text-[24px] text-black dark:text-white mt-10">
     {{ title }}
   </h3>
   <p class="subtitle font-[400] text-gray-600 mt-1 dark:text-gray-300">
